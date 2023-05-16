832797c8c2b4a06e3e2fc40c27e5852cb2cfc0eb
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-16 14:30:33 +0200

    Konwersja wartości

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,10 +5,10 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 
 //using (var context = new MyContext("Server=(local)\\SQLEXPRESS;Database=MyDb;User Id=sa;Password=pa$$w0rd"))
-using (var context = new MyContext("Server=(local)\\SQLEXPRESS;Database=MyDb;Integrated security=true"))
+using (var context = new MyContext("Server=(local)\\SQLEXPRESS;Database=MyDb;Integrated security=true;Encrypt=false"))
 //using (var context = new MyContext("Server=(localdb)\\mssqllocaldb;Database=MyDb;AttachDBFilename=c:\\EF6\\abc.mdf"))
 {
-    //var result = context.Database.EnsureDeleted();
+    var result = context.Database.EnsureDeleted();
 
 }
 
@@ -37,12 +37,30 @@ using (var context = new MyContext(options))
 
     values = context.Set<StandaloneAddress>().ToList();
 
-    values.First().City = "1";
+    /*values.First().City = "1";
     if (values.First().Location != null)
-        ;
+        ;*/
 
 }
 
-Console.WriteLine();
+
+using (var context = new MyContext(options))
+{
+    var user1 = new User { Username = "Admin", Password = "pasword", Type = UserTypes.Admin };
+    var user2 = new User { Username = "User", Password = "pasword", Type = UserTypes.User };
+    var user3 = new User { Username = "Anonymouse", Password = "pasword", Type = UserTypes.Anonymouse };
+
+    context.Add(user1);
+    context.Add(user2);
+    context.Add(user3);
+
+    context.SaveChanges();
+
+}
+
+
+
+
+    Console.WriteLine();
 
 
--- DAL/Configurations/UserConfiguration.cs
+++ DAL/Configurations/UserConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,17 @@ namespace DAL.Configurations
             builder.HasKey(x => x.Username);
 
             builder.Property(x => x.Username).HasDefaultValueSql("'user' + CAST(NEXT VALUE FOR NumbersSequence AS varchar)");
+
+
+            /*builder.Property(x => x.Type).HasConversion(
+                @enum => @enum.ToString(),
+                @string => Enum.Parse<UserTypes>(@string));*/
+
+            //builder.Property(x => x.Type).HasConversion(new EnumToStringConverter<UserTypes>());
+            builder.Property(x => x.Type).HasConversion<string>();
+
+            builder.Property(x => x.Password).HasConversion(x => Convert.ToBase64String(Encoding.Default.GetBytes(x)),
+                                                            x => Encoding.Default.GetString(Convert.FromBase64String(x)));
         }
     }
 }
